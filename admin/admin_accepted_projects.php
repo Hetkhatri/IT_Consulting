8e526e6919515828e12fe34ab9eac792de8f159f
Vansh <vanshhalvadiya675@gmail.com>
2025-02-11 13:15:47 +0530

    aa

--- admin/admin_accepted_projects.php
+++ admin/admin_accepted_projects.php
@@ -1,75 +1,132 @@
 <?php
-    session_start();
-    include('../Database/database_connectivity.php');
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <link rel="icon" type="image/x-icon" href="./favicon.ico">
-  <link rel="stylesheet" href="../css/admin_project.css">
-  <link rel="stylesheet" href="../src/css/adminlte.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-  <style>
-   
-  </style>
-</head>
-<body class="hold-transition sidebar-mini">
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+if (strlen($_SESSION['odmsaid']==0)) {
+  header('location:logout.php');
+  } else{
 
-  <?php
-     $sql = "select * from user_signup";
-     $result = $connection->query($sql);
+
+
   ?>
-  <div class="wrapper">
-    <?php include 'admin_header.php' ?>
-    
-    <div class="content-wrapper">
-    <h1 class="head">Registered Users</h1>
-    <?php
-     $sql = "select * from user_signup";
-     $result = $connection->query($sql);
-     ?>
-      <table class="project">
-        <tr>
-            <th> </th>
-            <th>Username</th>
-            <th>E-Mail</th>
-            <th>Contact-No.</th>
-            <th>password</th>
-        </tr>  
-        <?php
-    if ($result->num_rows > 0) 
-    {
-        // Output data of each row
-        while ($row = $result->fetch_assoc()) {
-          echo "<tr>
-          <td>" . $row["id"] . "</td>
-          <td>" . $row["username"] . "</td>
-          <td>" . $row["email"] . "</td>
-          <td>" . $row["contactno"] . "</td>
-          <td>" . $row["password"] . "</td>
-          </tr>";
-        }
-    } 
-    else 
-    {
-        echo "<tr><td colspan='4'>No records found</td></tr>";
-      }
-      ?>
-      </table>
-    </div>
+<!doctype html>
+<html lang="en" class="no-focus"> <!--<![endif]-->
+    <head>
+        <title>Onlind DJ Management System - New Booking</title>
 
-    <?php include ('admin_footer.php'); ?>
-  </div>
+        <!-- <link rel="stylesheet" href="assets/js/plugins/datatables/dataTables.bootstrap4.min.css"> -->
 
-  <!-- jQuery -->
-  <script src="./src/js/jquery.min.js"></script>
+        <link rel="stylesheet" id="css-main" href="assets/css/codebase.min.css">
 
-  <!-- Bootstrap 4 -->
-  <script src="./src/js/bootstrap.bundle.min.js"></script>
-  <script src="./src/js/adminlte.min.js"></script>
-</body>
+    </head>
+    <body>
+        
+        <div id="page-container" class="sidebar-o sidebar-inverse side-scroll page-header-fixed main-content-narrow">
+           
+           <?php include_once('includes/sidebar.php');?>
+
+          <?php include_once('includes/header.php');?>
+
+
+            <!-- Main Container -->
+            <main id="main-container">
+                <!-- Page Content -->
+                <div class="content">
+                    <h2 class="content-heading">New Booking</h2>
+
+                   
+
+                    <!-- Dynamic Table Full Pagination -->
+                    <div class="block">
+                        <div class="block-header block-header-default">
+                            <h3 class="block-title">New Booking</h3>
+                        </div>
+                        <div class="block-content block-content-full">
+                            <!-- DataTables init on table by adding .js-dataTable-full-pagination class, functionality initialized in js/pages/be_tables_datatables.js -->
+                            <table class="table table-bordered table-striped table-vcenter js-dataTable-full-pagination">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center"></th>
+                                        <th>Booking ID</th>
+                                        <th class="d-none d-sm-table-cell">Cutomer Name</th>
+                                        <th class="d-none d-sm-table-cell">Mobile Number</th>
+                                        <th class="d-none d-sm-table-cell">Email</th>
+                                        <th class="d-none d-sm-table-cell">Booking Date</th>
+                                        <th class="d-none d-sm-table-cell">Status</th>
+                                        <th class="d-none d-sm-table-cell" style="width: 15%;">Action</th>
+                                       </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+$sql="SELECT * from tblbooking where Status is null";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $row)
+{               ?>
+                                    <tr>
+                                        <td class="text-center"><?php echo htmlentities($cnt);?></td>
+                                        <td class="font-w600"><?php  echo htmlentities($row->BookingID);?></td>
+                                        <td class="font-w600"><?php  echo htmlentities($row->Name);?></td>
+                                        <td class="font-w600"><?php  echo htmlentities($row->MobileNumber);?></td>
+                                        <td class="font-w600"><?php  echo htmlentities($row->Email);?></td>
+                                        <td class="font-w600">
+                                            <span class="badge badge-primary"><?php  echo htmlentities($row->BookingDate);?></span>
+                                        </td>
+                                        <?php $bstatus=$row->Status; ?>
+                
+
+                                        <td ><?php if($bstatus==''):?>
+                                            <span class="badge badge-warning">Not Processed Yet</span>
+                                            <?php elseif($bstatus=='Approved'):?>
+                                              <span class="badge badge-success"><?php  echo htmlentities($bstatus);?></span>
+                                              <?php elseif($bstatus=='Approved'):?>
+ <span class="badge badge-danger"><?php  echo htmlentities($bstatus);?></span>
+                                          <?php endif;?>
+                                        </td>
+                                         <td class="d-none d-sm-table-cell"><a href="view-booking-detail.php?editid=<?php echo htmlentities ($row->ID);?>&&bookingid=<?php echo htmlentities ($row->BookingID);?>" class="btn btn-info btn-sm">View</a></td>
+                                    </tr>
+                                    <?php $cnt=$cnt+1;}} ?> 
+                                
+                                
+                                  
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <!-- END Dynamic Table Full Pagination -->
+
+                    <!-- END Dynamic Table Simple -->
+                </div>
+                <!-- END Page Content -->
+            </main>
+            <!-- END Main Container -->
+
+           <?php include_once('includes/footer.php');?>
+        </div>
+        <!-- END Page Container -->
+
+        <!-- Codebase Core JS -->
+        <script src="assets/js/core/jquery.min.js"></script>
+        <script src="assets/js/core/popper.min.js"></script>
+        <script src="assets/js/core/bootstrap.min.js"></script>
+        <script src="assets/js/core/jquery.slimscroll.min.js"></script>
+        <script src="assets/js/core/jquery.scrollLock.min.js"></script>
+        <script src="assets/js/core/jquery.appear.min.js"></script>
+        <script src="assets/js/core/jquery.countTo.min.js"></script>
+        <script src="assets/js/core/js.cookie.min.js"></script>
+        <script src="assets/js/codebase.js"></script>
+
+        <!-- Page JS Plugins -->
+        <!-- <script src="assets/js/plugins/datatables/jquery.dataTables.min.js"></script> -->
+        <!-- <script src="assets/js/plugins/datatables/dataTables.bootstrap4.min.js"></script> -->
+
+        <!-- Page JS Code -->
+        <!-- <script src="assets/js/pages/be_tables_datatables.js"></script> -->
+    </body>
 </html>
+<?php }  ?>
