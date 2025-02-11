2e5e24aabbcc5c9643268790fb51d263b5b2dde2
Samkit <samkitjain2809@gmail.com>
2025-02-11 13:08:30 +0530

    Responsive

--- admin/admin_home.php
+++ admin/admin_home.php
@@ -72,10 +72,7 @@
               </div>
 
             </div>
-          </div>
-
-        </div>
-        <div class="col-lg-3 col-6">
+            <div class="col-lg-3 col-6">
 
 <div class="small-box bg-success">
   <div class="inner">
@@ -86,9 +83,13 @@
   <div class="icon">
     <i class="ion ion-stats-bars"></i>
   </div>
-  <a href="admin_accepted_projects" class="small-box-footer">More info<i class="fas fa-arrow-circle-right"></i></a>
+  <a href="admin_accepted_projects.php" class="small-box-footer">More info<i class="fas fa-arrow-circle-right"></i></a>
 </div>
 </div>
+          </div>
+
+        </div>
+        
       </section>
     </div>
 
