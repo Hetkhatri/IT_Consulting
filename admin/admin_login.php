d755d2d463270e00013195ec28cdf84ce08b4756
Samkit <samkitjain2809@gmail.com>
2025-02-11 18:10:41 +0530

    Minor changes..!

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -10,6 +10,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../css/login.css">
   <title>Admin Login Page</title>
+
 </head>
 
 <body>
@@ -54,6 +55,7 @@
       </form>
     </div>
   </div>
+  <script src="../js/animation.js"></script>
 </body>
 
 </html>
