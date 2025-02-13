d40fb6db2f7136d42af69de32253e639a66e4b74
Vansh <vanshhalvadiya675@gmail.com>
2025-02-13 20:22:28 +0530

    joi leje

--- admin/SRS/srs_project.php
+++ admin/SRS/srs_project.php
@@ -5,39 +5,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="srs.css">
-    <title>Final Documentation</title>
+    <title>final Documentation</title>
 </head>
 
 <body class="flex">
     <div class="main flex">
-        <h1>Final Requirement Form</h1>
-        <form method="POST" class="flex" action="pdf.php">
-    <div class="introduction">
-        <h3>Introduction Of Your Project</h3>
-        <input type="text" placeholder="Project Title" name="titl" required>
-        <input type="text" placeholder="Purpose" name="purpo" required>
-        <label>Issued Date</label>
-        <input type="date" name="dat" required>
-    </div>
+        <h1>Final Requirment Form</h1>
+            <form class="flex" action="pdf.php" method="POST">
+                <div class="introduction">
+                <h3>Introduction Of Your Project</h3>
+                <input type="text" placeholder="Project Title" name="titl">
+                <input type="text" placeholder="Purpose" name="purpo">
+                <label>Issued Date</label>
+                <input type="date" name="dat">
+        </div>
 
-    <div class="reference">
-        <input type="text" placeholder="Any Reference" name="referenc">
-    </div>
+        <div class="reference">
+            <input type="text" placeholder="Any Reference" name="referenc">
+        </div>
 
-    <div class="requirements">
-        <label>Requirements</label>
-        <label>Functional</label>
-        <textarea name="refu"></textarea>
-        <label>Non-Functional</label>
-        <textarea name="nonfu"></textarea>
-    </div>
+        <div class="requirments">
+            <label>Requirments</label>
+            <label>Functional</label>
+            <textarea name="refu"></textarea>
+            <label>Non-Functional</label>
+            <textarea name="nonfu"></textarea>
+        </div>
 
-    <div class="submit">
-        <button type="submit" name="btn">Submit</button> <!-- Added name="btn" -->
-    </div>
-</form>
+        <div class="submit">
+            <button name="submit">Submit</button>
+        </div>
 
-    </div> <!-- Close the "main" div here -->
+
+
+        </form>
+    </div>
 </body>
 
 </html>
