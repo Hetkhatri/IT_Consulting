0816ec6f0b7f411b2f17d5b73ebcc7fd443d7734
Samkit <samkitjain2809@gmail.com>
2025-02-13 18:58:10 +0530

    Refactor freelancer signup page to use external CSS and improve structure

--- freelancer/freelancer_signup.php
+++ freelancer/freelancer_signup.php
@@ -1,237 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>signup Page</title>
-  <?php
-  include('../Database/database_connectivity.php')
-  ?>
-  <style>
-    body {
-      font-family: Arial, sans-serif;
-      margin: 0;
-      padding: 0;
-      background: linear-gradient(to right, #dff6f0, #f1fcf8);
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100vh;
-    }
 
-    .container {
-      width: 900px;
-      height: 500px;
-      display: flex;
-      box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
-      border-radius: 15px;
-      overflow: hidden;
-      background-color: #fff;
-    }
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/sign-up.css">
+    <title>signup Page</title>
+    <?php
+    include('../Database/database_connectivity.php')
+      ?>
 
-    /* Left Side - Welcome Section */
-    .welcome-section {
+  </head>
 
-      background:rgb(46, 104, 113);
-      color: #fff;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      padding: 40px;
-      text-align: center;
-      position: absolute;
-      top:115px;
-      right: 318px;
-      height: 57.6%;
-    }
-
-    .welcome-section h2 {
-      font-size: 28px;
-      margin-bottom: 10px;
-    }
-
-    .welcome-section p {
-      font-size: 16px;
-      margin-bottom: 20px;
-    }
-
-    .welcome-section button {
-      background-color: #fff;
-      color: rgb(46, 104, 113);
-      border: none;
-      padding: 12px 30px;
-      font-size: 16px;
-      border-radius: 20px;
-      cursor: pointer;
-      transition: all 0.3s ease;
-    }
-
-    .welcome-section button:hover {
-      background-color: #c2f3e9;
-    }
-
-    /* Right Side - Sign-Up Section */
-    .signup-section {
-      width: 50%;
-      padding: 40px;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      position: absolute;
-      left: 90px;
-      position: absolute;
-      top:80px;
-    }
-
-    .signup-section h2 {
-      font-size: 28px;
-      margin-bottom: 20px;
-    }
-
-    .signup-section .social-icons {
-      display: flex;
-      gap: 15px;
-      margin-bottom: 20px;
-    }
-
-    .signup-section .social-icons img {
-      width: 30px;
-      cursor: pointer;
-      transition: transform 0.3s ease;
-    }
-
-    .signup-section .social-icons img:hover {
-      transform: scale(1.2);
-    }
-
-    .signup-section form {
-      width: 100%;
-      max-width: 300px;
-      display: flex;
-      flex-direction: column;
-    }
-
-    .signup-section form input {
-      padding: 10px;
-      margin-bottom: 15px;
-      border: 1px solid #ccc;
-      border-radius: 5px;
-      font-size: 16px;
-    }
-
-    .signup-section form button {
-      background-color: rgb(46, 104, 113);
-      color: #fff;
-      border: none;
-      padding: 12px;
-      font-size: 16px;
-      border-radius: 5px;
-      cursor: pointer;
-      transition: background-color 0.3s ease;
-    }
-
-    .signup-section form button:hover {
-      background-color:rgb(34, 76, 82);
-    }
-
-    /* Animation */
-   
-     .welcome-section{
-      border-bottom-left-radius: 200px;
-      border-top-left-radius: 200px;
-     }
-      .animation_section{
-       background: rgb(46, 104, 113);
-      color: #fff;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      padding: 40px;
-      text-align: center;
-      position: absolute;
-      top: 0px;
-
-      left:35px;
-      height: 90%;
-      width: 80%;
-    }
-    .togglePassword{
-      height:25px;
-      width:25px;
-      position:absolute;
-      top:336px;
-      left:545px;
-    }
-    .back-image{
-      position: absolute;
-      left: 420px;
-      top:25px;
-      height:40px;
-      transition: transform 0.2s ease-in-out;
-    }
-    .back-image:hover{
-     height:40px;
-     position: absolute;
-      left: 420px;
-      transform: scale(1.2);
-      top:25px; 
-
-    }
-  </style>
-</head>
-<body>
-  <div class="container">
-    <!-- Left Side - Welcome Section -->
-    <div class="welcome-section">
-    <a href="../index.php"><img src="../includes/images/back.png" class="back-image"></a>
-      <h2>Welcome freelancer !</h2>
-      <p>Enter your personal details to use all of the site features.</p>
-      <a href ="freelancer_login.php"><button>Login In</button></a>
-    </div>
-
-    <!-- Right Side - Sign-Up Section -->
-    <div class="signup-section">
-      <h2>Create Account</h2>
-      <div class="social-icons">
-        <img src="https://img.icons8.com/color/48/google-logo.png"alt="Google" />
-        <img src="https://img.icons8.com/color/48/facebook.png" alt="Facebook" />
-        <img src="https://img.icons8.com/color/48/linkedin.png" alt="LinkedIn" />
+  <body class="flex">
+    <div class="container flex">
+      <!-- Left Side - Welcome Section -->
+      <div class="welcome-section flex">
+        <a href="../index.php"><img src="../includes/images/back.png" class="back-image"></a>
+        <h2>Welcome freelancer !</h2>
+        <p>Enter your personal details to use all of the site features.</p>
+        <a href="freelancer_login.php"><button>Log In</button></a>
       </div>
-      <form method="post">
-        <input type="username" placeholder="Username" required name="username"/>
-        <input type="email" placeholder="Email" required name="email"/>
-        <input type="text" placeholder="Phone number" name="phone">
-        <input type="password" placeholder="Password"  id="password" required name="password"/>
-        <img src="../includes/images/hide.png" class="togglePassword" id="togglePassword">
-        <input type="password" placeholder="Confirm Password" id="password" required />
-        <button type="submit" name="btn">Signup</button>
-      </form> 
-  </div>
-  <script src ="animation.js">
-  </script>
-  <?php
-    if(isset($_REQUEST['btn']))
-    {
-      $username = $_REQUEST['username'];
-      $email = $_REQUEST['email'];
-      $phone = $_REQUEST['phone'];
-      $password = $_REQUEST['password'];
-      $insert ="insert into freelancer_signup(username,email,contactno,password)values('".$username."','".$email."','".$phone."','".$password."')";
-      $execute = $connection->query($insert);
-      if($execute)
-      {
-        $message = "Account Created Successfully";
-        echo "<script type='text/javascript'>alert('$message');</script>";
+
+      <!-- Right Side - Sign-Up Section -->
+      <div class="signup-section flex">
+        <h2>Create Account</h2>
+        <div class="social-icons">
+          <img src="https://img.icons8.com/color/48/google-logo.png" alt="Google" />
+          <img src="https://img.icons8.com/color/48/facebook.png" alt="Facebook" />
+          <img src="https://img.icons8.com/color/48/linkedin.png" alt="LinkedIn" />
+        </div>
+        <form method="post" class="flex">
+          <input type="username" placeholder="Username" required name="username" />
+          <input type="email" placeholder="Email" required name="email" />
+          <input type="text" placeholder="Phone number" name="phone">
+          <div class="lock">
+            <input type="password" placeholder="Password" id="password" required name="password" />
+            <img src="../includes/images/hide.png" class="togglePassword" id="togglePassword">
+          </div>
+          <input type="password" placeholder="Confirm Password" id="password" required />
+          <button type="submit" name="btn">Signup</button>
+        </form>
+      </div>
+      <script src="../js/animation.js">
+      </script>
+      <?php
+      if (isset($_REQUEST['btn'])) {
+        $username = $_REQUEST['username'];
+        $email = $_REQUEST['email'];
+        $phone = $_REQUEST['phone'];
+        $password = $_REQUEST['password'];
+        $insert = "insert into freelancer_signup(username,email,contactno,password)values('" . $username . "','" . $email . "','" . $phone . "','" . $password . "')";
+        $execute = $connection->query($insert);
+        if ($execute) {
+          $message = "Account Created Successfully";
+          echo "<script type='text/javascript'>alert('$message');</script>";
+        } else {
+          $message = "Account Created unsuccessfully";
+          echo "<script type='text/javascript'>alert('$message');</script>";
+        }
       }
-      else
-      {
-        $message = "Account Created unsuccessfully";
-        echo "<script type='text/javascript'>alert('$message');</script>";
-      }
-    }
-  ?>
-</body>
+      ?>
+  </body>
+
 </html>
--- user/user_project.php
+++ user/user_project.php
@@ -6,104 +6,106 @@ include('user_header.php');
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <!-- <link rel="stylesheet" href="css\user_project.css"> -->
-    <style>
-        body {
-            background-image: #088F8F;
-        }
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <!-- <link rel="stylesheet" href="css\user_project.css"> -->
+        <style>
+            body {
+                background-image: #088F8F;
+            }
 
-        body {
-            margin: 0px;
-            padding: 0px;
-            box-sizing: border-box;
-            background-color:rgb(255, 255, 255);
+            body {
+                margin: 0px;
+                padding: 0px;
+                box-sizing: border-box;
+                background-color: rgb(255, 255, 255);
 
 
-        }
+            }
 
-        .form-container {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            flex-wrap: nowrap;
-            flex-direction: column;
-            height: 100vh;
-            width: 40vh;
-            margin-left: 80vh;
-            margin-top: -13vh;
-            border-radius: 15px;
-            border-top-left-radius: 50px;
-        }
+            .form-container {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-wrap: nowrap;
+                flex-direction: column;
+                height: 100vh;
+                width: 40vh;
+                margin-left: 80vh;
+                margin-top: -13vh;
+                border-radius: 15px;
+                border-top-left-radius: 50px;
+            }
 
-        .form-all{
-            /* background-color: #8EC5FC; */
-            /* background-image: linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 1s  0%); */
-            border-radius: 20px;
-            padding:20px;
-            margin-top:50px;
-            border:7px double black;
+            .form-all {
+                /* background-color: #8EC5FC; */
+                /* background-image: linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 1s  0%); */
+                border-radius: 20px;
+                padding: 20px;
+                margin-top: 50px;
+                border: 7px double black;
 
-        }
-        
-        .form-input input,select{
-            height: 6vh;
-            width: 50vh;
-            margin-bottom: 20px;
-            padding: 10px;
-            border: none;
-            outline: 0.5vh solid rgb(29, 29, 34);  
-            font-size: 1.5em;
-            background:rgb(195, 229, 234);
-            border-radius: 7px;
-        }
+            }
 
-        .h1 {
-            color: black;
-            font-size: 1.5em;
-            margin-bottom: 5vh;
-        }
+            .form-input input,
+            select {
+                height: 6vh;
+                width: 50vh;
+                margin-bottom: 20px;
+                padding: 10px;
+                border: none;
+                outline: 0.5vh solid rgb(29, 29, 34);
+                font-size: 1.5em;
+                background: rgb(195, 229, 234);
+                border-radius: 7px;
+            }
 
-        option{
-            background-color: #8EC5FC;
-            background-image: linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%);
-        }
+            .h1 {
+                color: black;
+                font-size: 1.5em;
+                margin-bottom: 5vh;
+            }
 
-        option:hover{
-            background-color: red;
-        }
+            option {
+                background-color: #8EC5FC;
+                background-image: linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%);
+            }
 
-        button {
-            margin-top: 2vh;
-            margin-left: 12vh;
-            padding: 0.1vh 7vh;
-            border-radius: ;
-            border: none;
-            font-size: 1.5em;
-            color: #fff;
-            background: rgb(59, 170, 187);
-            outline: 0.5vh solid rgb(28, 28, 34);  
-            font-weight: 700;
-            border-radius: 7px;
-        }
+            option:hover {
+                background-color: red;
+            }
 
-        button:hover{
-            background: rgb(52, 109, 117);
-            transition: 0.5s ease-in;
-        }
-        .tex{
-            margin-left: 53vh;
-            color: black;
-        }
-    </style>
-</head>
-<body>
-    <?php
+            button {
+                margin-top: 2vh;
+                margin-left: 12vh;
+                padding: 0.1vh 7vh;
+                border: none;
+                font-size: 1.5em;
+                color: #fff;
+                background: rgb(59, 170, 187);
+                outline: 0.5vh solid rgb(28, 28, 34);
+                font-weight: 700;
+                border-radius: 7px;
+            }
+
+            button:hover {
+                background: rgb(52, 109, 117);
+                transition: 0.5s ease-in;
+            }
+
+            .tex {
+                margin-left: 53vh;
+                color: black;
+            }
+        </style>
+    </head>
+
+    <body>
+        <?php
         if (isset($_SESSION['email'])) {
-            $email = $_SESSION['email'];           
+            $email = $_SESSION['email'];
             $query = "SELECT email FROM user_signup WHERE email = ?";
             $stmt = $connection->prepare($query);
             $stmt->bind_param("s", $email);
@@ -125,14 +127,14 @@ include('user_header.php');
         }
         //Contact No
         if (isset($_SESSION['email'])) {
-            $email = $_SESSION['email'];           
+            $email = $_SESSION['email'];
             // Database query to fetch username
             $query = "SELECT contactno FROM user_signup WHERE email = ?";
             $stmt = $connection->prepare($query);
             $stmt->bind_param("s", $email);
             $stmt->execute();
             $result = $stmt->get_result();
-        
+
             if ($result->num_rows > 0) {
                 $row = $result->fetch_assoc();
                 $contactno = $row['contactno'];
@@ -147,55 +149,54 @@ include('user_header.php');
             // header("Location: login.php");
             // exit();
         }
-    ?>
-    <br>
-    <h1 class="tex">
-        TELL US ABOUT YOUR PROJECT 🗒
-    </h1>
-    <div class="form-container">
-        <div class="form-all">
-       <h1 class="h1"> FILL THISH FORM ✍🏻</h1>
-        <form action="#" method="POST">
-            <div class="form-input">
-                <input type="text" placeholder="Project Title" required name="title">
-                <input type="text" placeholder="Enter project type" required name="type">
-                <input type="textarea" placeholder="Enter project details" required name="details">
-                <input type="email" placeholder="Enter Email" required value="<?php echo $email?>" name="email">
-                <input type="text" placeholder="Contact Number" required value="<?php echo $contactno?>" name="contactno">
-                <input type="text" placeholder="Budget" required name="budget">
+        ?>
+        <br>
+        <h1 class="tex">
+            TELL US ABOUT YOUR PROJECT 🗒
+        </h1>
+        <div class="form-container">
+            <div class="form-all">
+                <h1 class="h1"> FILL THIS FORM ✍🏻</h1>
+                <form action="#" method="POST">
+                    <div class="form-input">
+                        <input type="text" placeholder="Project Title" required name="title">
+                        <input type="text" placeholder="Enter project type" required name="type">
+                        <input type="textarea" placeholder="Enter project details" required name="details">
+                        <input type="email" placeholder="Enter Email" required value="<?php echo $email ?>"
+                            name="email">
+                        <input type="text" placeholder="Contact Number" required value="<?php echo $contactno ?>"
+                            name="contactno">
+                        <input type="text" placeholder="Budget" required name="budget">
+                    </div>
+                    <div class="submit">
+                        <button type="submit" name="btn">Submit</button>
+                    </div>
+                </form>
             </div>
-            <div class="submit">
-                <button type="submit" name="btn">Submit</button>
-            </div>
-        </form>
         </div>
-    </div>
-    <?php
-        if(isset($_REQUEST['btn']))
-        {
-          $title = $_REQUEST['title'];
-          $type = $_REQUEST['type'];
-          $details = $_REQUEST['details'];
-          $email = $_REQUEST['email'];
-          $contact_no = $_REQUEST['contactno'];
-          $budget = $_REQUEST['budget'];
-          $insert ="insert into user_project(title,type,details,email,contactno,budget)values('".$title."','".$type."','".$details."','".$email."','".$contactno."','".$budget."')";
-          $execute = $connection->query($insert);
-          if($execute)
-          {
-            $message = "Project submited successfully we will contact you soon";
-            echo "<script type='text/javascript'>alert('$message');</script>";
-          }
-          else
-          {
-            $message = "project not submited";
-            echo "<script type='text/javascript'>alert('$message');</script>";
-          }
+        <?php
+        if (isset($_REQUEST['btn'])) {
+            $title = $_REQUEST['title'];
+            $type = $_REQUEST['type'];
+            $details = $_REQUEST['details'];
+            $email = $_REQUEST['email'];
+            $contact_no = $_REQUEST['contactno'];
+            $budget = $_REQUEST['budget'];
+            $insert = "insert into user_project(title,type,details,email,contactno,budget)values('" . $title . "','" . $type . "','" . $details . "','" . $email . "','" . $contactno . "','" . $budget . "')";
+            $execute = $connection->query($insert);
+            if ($execute) {
+                $message = "Project submited successfully we will contact you soon";
+                echo "<script type='text/javascript'>alert('$message');</script>";
+            } else {
+                $message = "project not submited";
+                echo "<script type='text/javascript'>alert('$message');</script>";
+            }
         }
-      ?>
-      <footer>
-        <p>&copy; 2025 IT Consulting Platform. All rights reserved.</p>
-    </footer>
-      <link rel="stylesheet" href="../css/header.css">
-</body>
+        ?>
+        <footer>
+            <p>&copy; 2025 IT Consulting Platform. All rights reserved.</p>
+        </footer>
+        <link rel="stylesheet" href="../css/header.css">
+    </body>
+
 </html>
