fea869528222e0b98bae747e7ea59a5c047a6e54
mukund <mukundkachhadiya34@gmail.com>
2025-02-14 19:36:52 +0530

    hi

--- user/user_header.php
+++ user/user_header.php
@@ -67,7 +67,7 @@
                             <a class="nav-link" href="../index.php">Log out</a>
                         </li>
                         <li class="nav-item">
-                        <a href="../cooming_soon/index.html"> <img src="../includes/images/profile-user.png" alt="profile-user" style="height:50px;position:absolute; right:20px;top:30px"></a>
+                        <a href="../cooming_soon/index.html"> <img src="../includes/images/profile-user.png" alt="profile-user" style="height:50px;position:absolute; right:30px;top:25px"></a>
                         </li>
                         <!-- <li class="nav-item"> -->
                             <!-- <a class="nav-link" href="signup.php">signup</a> -->
